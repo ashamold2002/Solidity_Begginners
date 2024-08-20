2cb952feaad4f5400397c21a527366b5b7afc87a
ashamold2002 <ashamold2002@gmail.com>
2024-08-20 17:51:43 +0530

    Twitter_Array

--- contracts/ultimate-solidity-course-for-complete-beginners-main/Exercises/7-11-BasicTwitter-Exercise.sol
+++ contracts/ultimate-solidity-course-for-complete-beginners-main/Exercises/7-11-BasicTwitter-Exercise.sol
@@ -9,14 +9,17 @@ pragma solidity ^0.8.0;
 // 5️⃣ Add array of tweets 
 
 contract Twitter {
-    mapping (address=>string)public tweet;
+    mapping (address=>string[])public tweet;
 
     function createTweet(string memory _tweet)public{
-        tweet[msg.sender]=_tweet;
+        tweet[msg.sender].push(_tweet);
     }
     
-    function getTweet(address _address)public view returns(string memory){
-        return tweet[_address];
+    function getTweet(address _address,uint _i)public view returns(string memory){
+        return tweet[_address][_i];
+    }
+    function getAllTweet(address _add)public view returns(string[] memory ){
+        return tweet[_add];
     }
 
 }
